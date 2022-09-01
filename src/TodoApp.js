6466c74527ce11aa4ea534e00406f4b45a13c1f5
Raytene Han <raytenehan@gmail.com>
2022-09-01 14:47:52 -0700

    starter code + started adding functions

---
+++ src/TodoApp.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm";
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp() {
+  const [todoListData, setTodoListData] = useState([]);
+
+  /** add a new todo to list */
+  function create(newTodo) {
+    let newTodoWithId = { ...newTodo, id: uuid() };
+    setTodoListData(todos => [...todos, newTodoWithId]);
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+    setTodoListData(curr => curr.map(
+      todo => todo.id === updatedTodo.id ? updatedTodo : todo));
+  }
+
+  /** delete a todo by id */
+  function remove(id) {
+    setTodoListData(curr => curr.filter(todo => todo.id !== id));
+  }
+
+  return (
+      <main className="TodoApp">
+        <div className="row">
+
+          <div className="col-md-6">
+            <EditableTodoList
+                todos={todoListData}
+                update={update}
+                remove={remove}
+            /> OR
+            <span className="text-muted">You have no todos.</span>
+          </div>
+
+          <div className="col-md-6">
+            (if no top todo, omit this whole section)
+            <section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo />
+            </section>
+
+            <section>
+              <h3 className="mb-3">Add Nü</h3>
+              <TodoForm handleSave={create} />
+            </section>
+          </div>
+
+        </div>
+      </main>
+  );
+}
+
+export default TodoApp;
---
+++ src/TopTodo.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+/** Shows the top todo.
+ *
+ * Props:
+ * - todos
+ *
+ * TodoApp -> TopTodo
+ */
+
+function TopTodo({ todos }) {
+  // lowest-priority # is the highest priority
+  let top = todos.reduce(
+      (acc, cur) => cur.priority < acc.priority ? cur : acc, todos[0]);
+
+  const { id, title, description, priority } = top;
+  
+  return <Todo id={id}
+               title={title}
+               description={description}
+               priority={priority}  />;
+}
+
+export default TopTodo;
